dfdec7cf4f65c9972919b27bdc8de6f00bdef948
Fabian-Fedora <Fabian.neg3@gmail.com>
2025-07-12 11:50:18 +0200

    Entwicklung

--- tableSessions.py
+++ tableSessions.py
@@ -1,11 +1,15 @@
 import pandas as pd
+from appendSession import appendSession
 
+#TODO add function to append Session
 sessionDataSet ={
-    "Fach" :["Mehrrechner Datenbanken"],
-    "Aufgabe" :["SQL Tutorial"],
-    "Dauer" :[25],
-    "Datum" : ["11 Juli 2025"]}
+    "Fach" :["Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken"],
+    "Aufgabe" :["SQL Tutorial", "SQL Tutorial", "SQL Tutorial"],
+    "Dauer" :[25, 25, 25],
+    "Datum" : ["11 Juli 2025","11 Juli 2025","11 Juli 2025"]}
 
 sessionDataSet_var = pd.DataFrame(sessionDataSet)
 
 print(sessionDataSet_var)
+
+appendSession()
