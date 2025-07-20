58056c6c06196d97391abe9d3dc0d2a6adc4b9bb
Fabian-Fedora <Fabian.neg3@gmail.com>
2025-07-20 14:23:20 +0200

    Update

--- tableSessions.py
+++ tableSessions.py
@@ -3,10 +3,10 @@ from appendSession import appendSession
 
 #TODO add function to append Session
 sessionDataSet ={
-    "Fach" :["Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken"],
-    "Aufgabe" :["SQL Tutorial", "SQL Tutorial", "SQL Tutorial", "Vorlesungsfolien"],
-    "Dauer" :[25, 25, 25, 25],
-    "Datum" : ["11 Juli 2025","11 Juli 2025","11 Juli 2025", "12 Juli 2025"]}
+    "Fach" :["Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "Mehrrechner Datenbanken", "NOSQL", "NOSQL", "NOSQL"],
+    "Aufgabe" :["SQL Tutorial", "SQL Tutorial", "SQL Tutorial", "Vorlesungsfolien", "Vorlesungsvideo", "Vorlesungsvideo ; SQL Tutorial", "SQL Tutorial", "SQL Tutorial", "Vorlesungsvideo", "Vorlesungsvideo", "Vorlesungsvideo", "Vorlesungsvideo", "SQL Tutorial", "Vorlesungsvideo", "Vorlesungsvideo", "Vorlesungsvideo", "Vorlesungsvideo", "Vorlesungsvideo", "Klausurvorbereitung mit Kommilitone", "Wiederholung Klausurvorbereitung", "Lots", "Lots", "Lots", "Wiederholung Klausurvorbereitung", "Vorlesungsvideo", "Vorlesungsvideo", "Vorlesungsvideo"],
+    "Dauer" :[25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 120, 25, 25, 25, 25, 25, 25, 25, 25],
+    "Datum" : ["11 Juli 2025","11 Juli 2025","11 Juli 2025", "12 Juli 2025", "14 Juli 2025", "14 Juli 2025", "14 Juli 2025", "15 Juli 2025", "15 Juli 2025", "15 Juli 2025", "15 Juli 2025", "15 Juli 2025", "16 Juli 2025", "16 Juli 2025", "16 Juli 2025", "17 Juli 2025", "17 Juli 2025", "17 Juli 2025", "17 Juli 2025", "17 Juli 2025", "18 Juli 2025", "18 Juli 2025", "18 Juli 2025", "18 Juli 2025", "20 Juli 2025", "20 Juli 2025", "20 Juli 2025"]}
 
 sessionDataSet_var = pd.DataFrame(sessionDataSet)
 
